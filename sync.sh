2417734235b5cb47828038e4eaafb95b15051868
Shichao An <moleculeandrew@gmail.com>
2013-01-15 03:21:21 -0500

    update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,30 +1,46 @@
 #!/bin/bash
-script_foler=/root/newscapture/
-file_folder=/root/newscapture/local/
+script_folder=/home/anshichao/newscapture/
+file_folder=${script_folder}local/
 py_path=/usr/bin/python
-pywikipedia_path=/root/pywikipedia/
+pywikipedia=/root/pywikipedia/
 news_ref=news_ref.txt
 news_unref=news_unref.txt
-news_add=news_add.txt
+news_add=news_append.txt
 flag=flag
 wiki_site=wiki.ibeike.com
 
-echo "Syncing $news_ref and $news_unref to $wiki_site..."
+# Execute Python scripts to dump temp files
+$py_path ${script_folder}main.py
 
-# Import referenced news to Template:NewsAuto
-$py_path ${pywikipedia}pagefromfile.py -start:AAAA -end:BBBB -titlestart:TTTT -titleend:TTTT -file:$file_folder$news_ref
+if [ "$?" -ne 0 ]
+then 
+	exit 1
+else
 
-# Import referenced news to Template:NewsAutoUntagged
-$py_path ${pywikipedia}pagefromfile.py -start:AAAA -end:BBBB -titlestart:TTTT -titleend:TTTT -file:$file_folder$news_unref
+	
+	# Check the size of flag file
+	fsize=$(cat $file_foler$flag | wc -c)
+	if [ "$fsize" -gt 2 ]
+	then
+		echo "Syncing $news_ref and $news_unref to $wiki_site..."
+		
+		# Import referenced news to Template:NewsAuto
+		$py_path ${pywikipedia}pagefromfile.py -start:AAAA -end:BBBB -titlestart:TTTT -titleend:TTTT -file:$file_folder$news_ref -force -notitle	
+		# Import referenced news to Template:NewsAutoUntagged
+		$py_path ${pywikipedia}pagefromfile.py -start:AAAA -end:BBBB -titlestart:TTTT -titleend:TTTT -file:$file_folder$news_unref -force -notitle
+		
+		echo "Syncing $news_add to $wiki_site..."
+		
+		# Add referenced appended news to Template:NewsUpdate
+		$py_path ${pywikipedia}add_text.py -textfile:$file_folder$news_add -page:Template:NewsUpdate -bottom -always
 
-# Check the size of flag file
-fsize=$(cat $file_foler$flag | wc -c)
-if [ "$fsize" -gt 2 ]
-then
-	echo "Syncing $news_add to $wiki_site..."
+		echo "Sync accomplished."
+		
+	else
+		echo "No newly dumped files detected. Sync terminated."
+	fi
+
+	echo
 fi
-
-echo "Sync accomplished."
-echo
 exit 0
 
