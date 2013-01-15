ac9eb1595ff49e289eee290a492cef0087c94f2b
Shichao An <moleculeandrew@gmail.com>
2013-01-15 03:52:03 -0500

    modify variables

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import news
 from conf import *
 def main():
-    dir       = FILE_DIR
+    directory = FILE_DIR
     url       = INDEX_URL
     num       = NEWS_NUM
     pattern   = ID_PATTERN
@@ -10,7 +10,7 @@ def main():
     api_url   = API_URL
     
     # Creating Wikinews object and invoke start()
-    wiki = news.WikiNews(dir, url, num, pattern, q_pattern, api_url)
+    wiki = news.WikiNews(directory, url, num, pattern, q_pattern, api_url)
     wiki.start()
 
 if __name__ == '__main__':
